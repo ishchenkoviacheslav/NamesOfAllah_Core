24cf1cc046edd8ea0f39550ffd0d8a0170f348d4
ishchenkoviacheslav <ishchenkoviacheslav@live.com>
2017-07-11 16:20:49 +0300

    Add files via upload

---
+++ NamesOfAllah_Core/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NamesOfAllah_Core.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            //чтото новое
+            ViewData["Message"] = "Your application description page.";
+            ViewData["Names"] = (new namesOfAllah.Models.Names()).ListOfNames.ToList();
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ NamesOfAllah_Core/Models/Name.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace namesOfAllah.Models
+{
+    public class Name
+    {
+        public string One_Name { get; set; }
+        public string Description { get; set; }
+    }
+}
